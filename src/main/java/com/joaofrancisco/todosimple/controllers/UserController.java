8c7303c896ebc317d86266e2df11d1651a6df80b
João Francisco <JF756061@gmail.com>
2026-04-20 15:58:24 -0300

    Create UserController

---
+++ src/main/java/com/joaofrancisco/todosimple/controllers/UserController.java
@@ -0,0 +1,63 @@
+package com.joaofrancisco.todosimple.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.joaofrancisco.todosimple.models.User;
+import com.joaofrancisco.todosimple.models.User.CreateUser;
+import com.joaofrancisco.todosimple.models.User.UpdateUser;
+import com.joaofrancisco.todosimple.services.UserService;
+
+@RestController // annotation do controller de uma api rest
+@RequestMapping("/user") // toda rota tem que seguir um padrao, comecando com user
+@Validated // Normalmente uma das funcoes do controller é validar, e essa annotation faz isso
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    // localhost:808/user/id(1)
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = this.userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping 
+    @Validated(CreateUser.class) // Vai ver se cumpre todas validacoes que colocamos no User em models
+    public ResponseEntity<Void> create(@Valid @RequestBody User obj){
+        this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/(id").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated(UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id){
+        obj.setId(id);
+        this.userService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
